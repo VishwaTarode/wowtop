b2d015fcfbb0f50852074b7234f0562b8f961d47
Lemu1717 <lemu@tarode.com>
2025-10-09 19:47:46 +0530

    UPDATE 20250915

---
+++ js/floatingsub.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const footerHTML = `
+    <div class="kefu-cb hidden-xs hidden-sm">
+  <ul>
+    <li>
+      <a href="../contact.html" rel="nofollow" target="_blank">
+        <div class="item">
+          <span></span>
+        </div>
+        <p>Online consultation</p>
+      </a>
+    </li>
+    <li>
+      <div class="item">
+        <span></span>
+      </div>
+      <p>Telephone consultation</p>
+      <div class="kefu-cb-nr">
+      <div class="ftel">
+        <b>Shenzhen</b>
+        Monday to Friday 09:00-18:30
+        0755-23732760        <b>hubei</b>
+        Monday to Friday 08:00-17:30
+        0722-7025001
+        <b>Huizhou</b>
+         Monday to Friday 08:00-17:30
+        0752-2168129
+        <b>business</b>
+       
+        Miss Luo 181 2621 7912
+        
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="item">
+        <span></span>
+      </div>
+      <p>WeChat consultation</p>
+      <div class="kefu-cb-nr kefu-cb-wxh">
+        <dl>
+          <dt>Official WeChat</dt>
+          <dd><img src="../uploads/img1/20220520/6287508ca6e2e.jpg"></dd>
+        </dl>
+        <dl>
+          <dt>Group business</dt>
+          <dd><img src="../uploads/abc1/20231219/65813b8620333.png"></dd>
+          <dd></dd>
+        </dl>      </div>
+    </li>
+    <li class="product-top">
+      <div class="item">
+        <span></span>
+      </div>
+    </li>
+  </ul>
+</div>
+    `;
+  
+    document.body.insertAdjacentHTML("beforeend", footerHTML);
+  });
+  
